b928aed12ebc249a2c714a28b0a97bc24b04a993
Abdelaziz Elrashed <aeemh.sdn@gmail.com>
2024-06-23 10:28:08 +0300

    تحديد أرقام نسخ لغة البايثون التي تدعمها المكتبة

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from setuptools import find_packages, setup
 setup(
     name='zakat',
     packages=find_packages(include=['zakat']),
-    version='0.1.0',
+    version='0.1.1720',
     description='This module provides the ZakatTracker class for tracking and calculating Zakat.',
     author='Abdelaziz Elrashed Elshaikh Mohamed',
     install_requires=[],
+    python_requires=">=3.10",
     setup_requires=['pytest-runner'],
     tests_require=['pytest==8.2.2'],
     test_suite='tests',
