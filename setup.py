5f1e2499423a7468891fd0f2b47bb18424f02db0
Abdelaziz Elrashed <aeemh.sdn@gmail.com>
2024-10-25 07:24:30 +0300

    Revert "دعم اصدارة بايثون 3.9"
    This reverts commit a3d491971538dacab1ac5c94af0e4c71ee609401.

--- setup.py
+++ setup.py
@@ -10,14 +10,13 @@ setup(
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires=">=3.9",
+    python_requires=">=3.10",
     packages=find_packages(
         include=[
             'zakat',
