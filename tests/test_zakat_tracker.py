bf4d02f3516ea3ab54222f1a94f65dbf35a584af
Abdelaziz Elrashed <aeemh.sdn@gmail.com>
2025-03-12 15:59:22 +0300

    اصلاح مسار الاختيارات

--- tests/test_zakat_tracker.py
+++ tests/test_zakat_tracker.py
@@ -1,6 +1,14 @@
 import os
 import time
-from zakat import test
+from zakat import ZakatTracker
 
 def test_zakat_tracker():
-    test(debug=True)
+    start = time.time_ns()
+    os.chdir('tests')
+    ledger = ZakatTracker('./zakat_test_db')
+    ledger.test(True)
+    print("#########################")
+    print("######## TEST DONE ########")
+    print("#########################")
+    print(ZakatTracker.duration_from_nanoseconds(time.time_ns() - start))
+    print("#########################")
